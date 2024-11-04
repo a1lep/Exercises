8c62094a40746e5b7e5556d2f5bc92b7dcc898d6
a1lep <aile.pent@gmail.com>
2024-11-04 15:27:37 +0200

    Hello

--- src/DayOne.java
+++ src/DayOne.java
@@ -1,7 +1,38 @@
 public class DayOne {
     public static void main(String[] args) {
 
-        System.out.println("Aile");
+
+        // Task 1 - if,else if, else
+         int a = -1;
+         int b = -3;
+
+        if ((b > a) && (a > 0)) {
+            System.out.println("b is bigger than a, and a is positive");
+        }
+
+        else if ((a > b) && (b > 0)) {
+            System.out.println("a is bigger than b, and b is positive");
+        }
+
+        else if ((a == b) || (b < 0) && (a < 0)) {
+            System.out.println("a and b are either equal or both are negative");
+        }
+
+        else {
+            System.out.println("No specific conditions were met");
+        }
+
+         /* if (b > a) {
+            System.out.println("b is bigger than a");
+        }
+        else if (a > b) {
+            System.out.println("a is bigger than b");
+        }
+
+         else {
+             System.out.println("a and b are equal");
+         }
+             */
 
     }
 
