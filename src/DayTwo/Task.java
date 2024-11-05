5f402c5db51a0eb0fef6674be7253d6c5af417f6
a1lep <aile.pent@gmail.com>
2024-11-05 11:12:54 +0200

    Task - odd or even, simple calculator, print

---
+++ src/DayTwo/Task.java
@@ -0,0 +1,31 @@
+package DayTwo;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        //TASK 1 - regular print
+        System.out.println("Hello World!");
+
+        // TASK 2 - arithmetic operations
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+
+        // TASK 3 - odd or even number
+        System.out.print("Enter number: ");
+        int a = scanner.nextInt();
+        if (a % 2 == 0) {
+            System.out.println("Your number is even");
+        }
+        else {
+            System.out.println("Your number is odd");
+        }
+    }
+}
