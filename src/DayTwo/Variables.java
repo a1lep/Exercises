446baf0385dfd9163c3b02268f2fb3346ab03d5f
a1lep <aile.pent@gmail.com>
2024-11-05 14:23:37 +0200

    variables 3

---
+++ src/DayTwo/Variables.java
@@ -0,0 +1,25 @@
+package DayTwo;
+
+public class Variables {
+        int number;
+        static String name = "John";
+        private static int totalAccounts = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println(totalAccounts);
+    }
+        public double add() {
+            double balance = 2;
+            System.out.println(totalAccounts);
+            return balance;
+        }
+
+        public double withdraw() {
+        double a = 2;
+        double b = 4;
+        return a*b;
+         }
+
+
+}
