ed7d587d23d2edd7afea39bea747c63d0cc40853
a1lep <aile.pent@gmail.com>
2024-11-05 11:13:25 +0200

    scanner

---
+++ src/DayTwo/DayTwo1.java
@@ -0,0 +1,16 @@
+package DayTwo;
+
+import java.util.Scanner;
+
+public class DayTwo1 {
+
+    public static void main(String[] args) {
+ // TASK 1 - scanner class import
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int n = scanner.nextInt();
+        System.out.println(n);
+
+
+    }
+}
