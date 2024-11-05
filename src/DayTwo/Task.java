65c59a65ce23e0b91d6077d825432bc076c86cf7
a1lep <aile.pent@gmail.com>
2024-11-05 12:32:25 +0200

    scanner

--- src/DayTwo/Task.java
+++ src/DayTwo/Task.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         //TASK 1 - regular print
-        System.out.println("Hello World!");
+        /* System.out.println("Hello World!");
 
         // TASK 2 - arithmetic operations
         Scanner scanner = new Scanner(System.in);
@@ -13,19 +13,38 @@ public class Task {
         int num1 = scanner.nextInt();
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
-        System.out.println(num1+num2);
-        System.out.println(num1-num2);
-        System.out.println(num1*num2);
-        System.out.println(num1/num2);
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
 
         // TASK 3 - odd or even number
-        System.out.print("Enter number: ");
-        int a = scanner.nextInt();
-        if (a % 2 == 0) {
+        System.out.print("Enter one number: ");
+        int n = scanner.nextInt();
+        if (n % 2 == 0) {
             System.out.println("Your number is even");
         }
         else {
             System.out.println("Your number is odd");
+        } */
+
+        // TASK 4 - greatest number
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int c = sc.nextInt();
+
+        if (a>b && a>c) {
+            System.out.println("The greatest number is " + a);
+        }
+        else if (b>a && b>c) {
+            System.out.println("The greatest number is " + b);
+        }
+        else {
+            System.out.println("The greatest number is " + c);
         }
     }
 }
