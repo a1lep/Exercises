c8f026348e640930f909679f0d24d9134ce0d9be
a1lep <aile.pent@gmail.com>
2024-11-06 09:23:47 +0200

    String exercises 5.11 w comments

--- src/DayTwo/StringExercises.java
+++ src/DayTwo/StringExercises.java
@@ -4,27 +4,27 @@ public class StringExercises {
     public static void main(String[] args) {
 
         String original = "hello world";
-        //Exercise 1
+        //Exercise 1 - läbivad suured tähed
         System.out.println(original.toUpperCase());
-        // Exercise 2
+        // Exercise 2 - läbivad väiksed tähed
         System.out.println(original.toLowerCase());
-        // Exercise 3
+        // Exercise 3 - kas sisaldab
         System.out.println(original.contains("world"));
-        // Exercise 4
+        // Exercise 4 -esimene täht
         System.out.println(original.charAt(0));
-        // Exercise 5
+        // Exercise 5 - asendamine
         System.out.println(original.replace('o', 'a'));
-        //Exercise 6
+        //Exercise 6 - algus ja lõpp sõna
         String hello = "Hello, world!";
         System.out.println(hello.startsWith("Hello"));
         System.out.println(hello.endsWith("world!"));
-        //Exercise 7
+        //Exercise 7 - vahemik
         System.out.println(hello.substring(7,12));
-        //Exercise 8
+        //Exercise 8 - tähe esimene ja viimane occurence
         String name = "abracadabra";
         System.out.println(name.lastIndexOf('a'));
         System.out.println(name.indexOf('a'));
-        //Exercise 9
+        //Exercise 9 - asendus (sõna)
         String java = "Java is fun";
         System.out.println(java.replace("fun", "awesome"));
     }
