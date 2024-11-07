04ac403a3b56df86fe3993def70e6af7fab13aa5
a1lep <aile.pent@gmail.com>
2024-11-07 21:36:47 +0200

    Break and continue exercises 7.11

--- src/DayFour/Break_Continue_exercise.java
+++ src/DayFour/Break_Continue_exercise.java
@@ -16,6 +16,23 @@ public class Break_Continue_exercise {
         }
 
         //Exercise 2 - Print Odd Numbers from 1 to 20, Skip Multiples of 5
+        for (int num = 1; num <=20; num = num + 2) {
+            if (num % 5 == 0){
+                continue;
+            }
+            System.out.println(num);
+        }
 
+        //Exercise 3 - Print Multiples of 3 up to 30, skip Multiples of 6
+        for (int a = 1; a <=30; a++) {
+            if (a % 3 == 0){
+                if (a % 6 == 0){
+                    continue;
+                }
+
+            System.out.println(a);
+
+            }
+        }
     }
 }
