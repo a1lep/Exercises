c63e81eb3db3993c146597890cc5fafd86eec592
a1lep <aile.pent@gmail.com>
2024-11-11 15:02:54 +0200

    Exercises 11.11 - Compound Interest Calculator

--- src/swDayOne/Additional_task.java
+++ src/swDayOne/Additional_task.java
@@ -7,7 +7,7 @@ public class Additional_task {
         // Additional task - Compound Interest Calculator (liitintressi kalkulaator)
         // A = (principal + interest ; P = initial amount of money (amount)
         //  r = annual interest rate (0,05); n = number of times the interest is compounded
-        // t = time (in years, months) (years)
+        // t = time (in years, months) (years).
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the principal amount of money: ");
         int amount = sc.nextInt();
