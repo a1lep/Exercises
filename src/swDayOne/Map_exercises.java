8c4d9ecd7a608fcff3eb86875dc2a2ed1388ea28
a1lep <aile.pent@gmail.com>
2024-11-11 14:55:30 +0200

    Exercises 11.11 - MapHash

--- src/swDayOne/Map_exercises.java
+++ src/swDayOne/Map_exercises.java
@@ -5,25 +5,37 @@ import java.util.Map;
 
 public class Map_exercises {
     public static void main(String[] args) {
-        // EXAMPLE
-        Map<String, Integer> ages = new HashMap<>();
-        ages.put("John", 20);
-        ages.put("Jane", 25);
-        ages.put("Mary", 30);
+        example();
 
-        for (String name : ages.keySet()) {
-            System.out.println(name + " is " + ages.get(name) + " years old.");
+        positions();
+
+        grades();
+
+        salaries();
+
+
+    }
+
+    private static void salaries() {
+        // Exercise 3 - Remove a Key-Value Pair
+        Map<String, Integer> salaries = new HashMap<>();
+        salaries.put("Jan", 2000);
+        salaries.put("Ester", 2500);
+        salaries.put("Mary", 3000);
+        salaries.put("Jack", 2500);
+
+        System.out.println("Original salaries:" + salaries);
+
+        String employeeToRemove = "Mary";
+        salaries.remove(employeeToRemove);
+
+        for (String name : salaries.keySet()) {
+            System.out.println(name + "' s salary is " + salaries.get(name));
         }
 
-        // Exercise 1 - Add User to Map - name + position, 2 Users
-        Map<String, String> positions = new HashMap<>();
-        positions.put("John", "teacher");
-        positions.put("Jane", "accountant");
+    }
 
-        for (String name : positions.keySet()) {
-            System.out.println(name + " is " + positions.get(name) + ".");
-        }
-
+    private static void grades() {
         // Exercise 2 - Store and Retrieve Student Grades
         Map<String, Integer> grades = new HashMap<>();
         grades.put("John", 5);
@@ -35,26 +47,28 @@ public class Map_exercises {
         System.out.println("Jane's grade is " + grades.get("Jane"));
         System.out.println("Jack's grade is " + grades.get("Jack"));
         System.out.println("Jackson's grade is " + grades.get("Jackson"));
+    }
 
-        // Exercise 3 - Remove a Key-Value Pair
-        Map<String, Integer> salaries = new HashMap<>();
-        salaries.put("Jan", 2000);
-        salaries.put("Ester", 2500);
-        salaries.put("Mary", 3000);
-        salaries.put("Jack", 2500);
+    private static void positions() {
+        // Exercise 1 - Add User to Map - name + position, 2 Users
+        Map<String, String> positions = new HashMap<>();
+        positions.put("John", "teacher");
+        positions.put("Jane", "accountant");
 
-        salaries.remove("Mary");
+        for (String name : positions.keySet()) {
+            System.out.println(name + " is " + positions.get(name) + ".");
+        }
+    }
 
-        for (String name : salaries.keySet()) {
-            System.out.println(name + "' s salary is " + salaries.get(name));
-        };
+    private static void example() {
+        // EXAMPLE
+        Map<String, Integer> ages = new HashMap<>();
+        ages.put("John", 20);
+        ages.put("Jane", 25);
+        ages.put("Mary", 30);
 
-
-
-
-
-
-
-
+        for (String name : ages.keySet()) {
+            System.out.println(name + " is " + ages.get(name) + " years old.");
+        }
     }
 }
